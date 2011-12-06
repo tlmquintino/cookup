023c401a9384b432d173b22eb0083b19e4cc05a6
Tiago Quintino <tlmquintino@gmail.com>
2011-12-06 23:37:59 +0000

    recipe for wget

---
+++ cookbook/wget.pm
@@ -0,0 +1,36 @@
+package wget;
+
+use strict;
+use warnings;
+
+use Recipe;
+
+my %fields = (
+    name     => "wget",
+    version  => "1.13.4",
+		url      => "http://ftp.gnu.org/gnu/wget/wget-1.13.4.tar.gz",
+		md5      => "1df489976a118b9cbe1b03502adbfc27",
+);
+
+our @ISA = ("Recipe");
+
+    sub new 
+		{
+        my $class = shift;
+        my $self  = $class->SUPER::new();
+        my($element);
+        foreach $element (keys %fields) {
+            $self->{_permitted}->{$element} = $fields{$element};
+        }
+        @{$self}{keys %fields} = values %fields;
+        return $self;
+    }
+
+		# string for the configure command
+		sub configure_command {
+			my $self = shift;
+			return "./configure --without-ssl --prefix=" . $self->prefix;
+		}
+
+
+1; 
