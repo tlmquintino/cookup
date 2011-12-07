7d8410fad12ace9bf0ac219b7e84ec1bb5fba4d5
Tiago Quintino <tlmquintino@gmail.com>
2011-12-07 11:20:33 +0000

    recipe for grib_api

---
+++ cookbook/grib_api.pm
@@ -0,0 +1,34 @@
+package grib_api;
+
+use strict;
+use warnings;
+
+use Recipe;
+
+my %fields = (
+    name     => "grib_api",
+    version  => "1.9.9",
+		url      => "http://www.ecmwf.int/products/data/software/download/software_files/grib_api-1.9.9.tar.gz",
+		md5      => "fe6c684e4a41477f3a6e97ab8892f35d",
+);
+
+our @ISA = ("Recipe");
+
+    sub new 
+		{
+        my $class = shift;
+        my $self  = $class->SUPER::new();
+        my($element);
+        foreach $element (keys %fields) {
+            $self->{_permitted}->{$element} = $fields{$element};
+        }
+        @{$self}{keys %fields} = values %fields;
+        return $self;
+    }
+
+		sub configure_command {
+			my $self = shift;
+			return "./configure --disable-jpeg --prefix=" . $self->prefix;
+		}
+
+1; 
