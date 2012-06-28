8b4fe22b3cceb5bb64330d1caf81b4305647b18e
Tiago Quintino <tlmquintino@gmail.com>
2012-06-28 11:08:46 +0100

    added glog

---
+++ cookbook/glog.pm
@@ -0,0 +1,34 @@
+# © Copyright 2011-2012 Tiago Quintino
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+
+package glog;
+
+use strict;
+use warnings;
+
+use Recipe;
+
+my %fields = (
+    name     => "glog",
+    version  => "0.3.2",
+		url      => "http://google-glog.googlecode.com/files/glog-0.3.2.tar.gz",
+		md5      => "897fbff90d91ea2b6d6e78c8cea641cc",
+);
+
+our @ISA = ("Recipe");
+
+    sub new
+		{
+        my $class = shift;
+        my $self  = $class->SUPER::new();
+        my($element);
+        foreach $element (keys %fields) {
+            $self->{_permitted}->{$element} = $fields{$element};
+        }
+        @{$self}{keys %fields} = values %fields;
+        return $self;
+    }
+
+1;
