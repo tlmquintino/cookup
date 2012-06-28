d3c4182a2490d21114ac3a19da119054077166ca
Tiago Quintino <tlmquintino@gmail.com>
2012-06-28 09:36:59 +0100

    add google test

---
+++ cookbook/gtest.pm
@@ -0,0 +1,39 @@
+# © Copyright 2011-2012 Tiago Quintino
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+
+package gtest;
+
+use strict;
+use warnings;
+
+use Recipe;
+
+my %fields = (
+    name     => "gtest",
+    version  => "1.6.0",
+		url      => "http://googletest.googlecode.com/files/gtest-1.6.0.zip",
+		md5      => "4577b49f2973c90bf9ba69aa8166b786",
+);
+
+our @ISA = ("Recipe");
+
+    sub new
+		{
+        my $class = shift;
+        my $self  = $class->SUPER::new();
+        my($element);
+        foreach $element (keys %fields) {
+            $self->{_permitted}->{$element} = $fields{$element};
+        }
+        @{$self}{keys %fields} = values %fields;
+        return $self;
+    }
+
+    sub configure_command {
+        my $self = shift;
+        return "cmake"; # gtest does not install
+    }
+
+1;
